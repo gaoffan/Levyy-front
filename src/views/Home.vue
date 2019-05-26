0548142af3aa01f526a98369ed04b90eb084b591
goufaan <suzukaze.kouko@gmail.com>
2019-05-26 18:42:14 +0800

    HomeView!

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,60 +3,46 @@
     <v-card>
       <v-list two-line subheader>
         <v-subheader inset>作业列表</v-subheader>
-
-        <v-list-tile
-                v-for="item in items"
-                :key="item.name"
-                avatar
-                @click="viewHomework(item.hid)"
-        >
+        <v-list-tile v-for="item in items"  :key="item.id" avatar @click="viewHomework(item.hid)">
           <v-list-tile-avatar>
             <v-icon class="blue lighten-1 white--text">{{ item.icon }}</v-icon>
           </v-list-tile-avatar>
-
           <v-list-tile-content>
             <v-list-tile-title>{{ item.name }}</v-list-tile-title>
             <v-list-tile-sub-title>{{ item.subtitle }}</v-list-tile-sub-title>
           </v-list-tile-content>
-
         </v-list-tile>
       </v-list>
+      <v-subheader>不会显示已截止超过 3 天的作业</v-subheader>
     </v-card>
   </v-flex>
 </template>
 
 <script>
-    export default {
-  name: 'home',
+  export default {
+    name: 'home',
     data(){
       return {
-          items: []
+        items: []
       }
     },
     methods:{
-        viewHomework(hid){
-            this.$router.push({ name: 'detail', params: { hid: hid }})
-        }
+      viewHomework(hid){
+        this.$router.push({ name: 'detail', params: { hid: hid }})
+      }
     },
     created(){
-        let items = this.items;
-        let ajax = new XMLHttpRequest()
-        ajax.open("GET","http://127.0.0.1:8080/api/getlist",true)
-        ajax.onload = function() {
-            if (ajax.readyState == 4 && ajax.status == 200) {
-                let result = JSON.parse(ajax.responseText)
-                console.log(result);
-                result.data.map(function(item){
-                    items.push({
-                        hid: item.id,
-                        icon: item.name[0],
-                        name: item.name,
-                        subtitle: "由 " + item.owner + " 发布于 " + new Date(item.deadline).Format("yyyy-MM-dd HH:mm:ss"),
-                    });
-                })
-            }
-        }
-        ajax.send()
+      let items = this.items;
+      this.$doAjax("GET","/api/getlist",function (result) {
+        result.data.map(function(item){
+          items.push({
+            hid: item.id,
+            icon: item.name[0],
+            name: item.name,
+            subtitle: item.owner + " - " + item.deadline_format + "截止",
+          });
+        })
+      });
     }
-}
+  }
 </script>
