32c73e625039f7d6a8410e96445facbd47f0ec59
goufaan <suzukaze.kouko@gmail.com>
2019-05-30 17:16:57 +0800

    fix

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
-export const CONFIG = {
+export const CONFIG_DBG = {
     apiUrl: "http://127.0.0.1:8080"
 };
-export const CONFIG_Release = {
+export const CONFIG = {
     apiUrl: "/homeworkupload"
 };
 Date.prototype.Format = function(fmt)
